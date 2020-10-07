f12852f7bf12839d8e72401c925f6308fa4cc8aa
T-Sumida <ui.suto05@gmail.com>
2020-10-07 23:00:26 +0900

    [update] transformsを使う仕様に変更

--- src/dataset.py
+++ src/dataset.py
@@ -2,47 +2,12 @@
 
 from typing import List, Dict, Tuple
 
-import cv2
 import librosa
 import numpy as np
 import torch.utils.data as data
 
 import augment
-
-
-def normalize(X: np.ndarray, mean=None, std=None, norm_max=None, norm_min=None, eps=1e-6) -> np.ndarray:
-    """画像の正規化
-
-    Args:
-        X (np.ndarray): 3chの画像
-        mean ([type], optional): 平均. Defaults to None.
-        std ([type], optional): 標準偏差. Defaults to None.
-        norm_max ([type], optional): 正規化最大値. Defaults to None.
-        norm_min ([type], optional): 正規化最小値. Defaults to None.
-        eps ([type], optional): 下駄. Defaults to 1e-6.
-
-    Returns:
-        np.ndarray: 正規化された3ch画像
-    """
-    # Standardize
-    mean = mean or X.mean()
-    X = X - mean
-    std = std or X.std()
-    Xstd = X / (std + eps)
-    _min, _max = Xstd.min(), Xstd.max()
-    norm_max = norm_max or _max
-    norm_min = norm_min or _min
-    if (_max - _min) > eps:
-        # Normalize to [0, 255]
-        V = Xstd
-        V[V < norm_min] = norm_min
-        V[V > norm_max] = norm_max
-        V = 255 * (V - norm_min) / (norm_max - norm_min)
-        V = V.astype(np.uint8)
-    else:
-        # Just zero
-        V = np.zeros_like(Xstd, dtype=np.uint8)
-    return V
+from transformer import Signal2ImageTransformer
 
 
 class SpectrogramDataset(data.Dataset):
@@ -60,10 +25,10 @@ class SpectrogramDataset(data.Dataset):
         """
         self.file_list = file_list
         self.img_size = settings['img_size']
-        self.target_sr = settings['target_sr']
         self.period = settings['period']
-        self.n_mels = settings['n_mels']
         self.labels = labels
+        self.transforms = Signal2ImageTransformer(
+            settings['target_sr'], settings['n_mels'])
         self.waveform_transforms = waveform_transforms
         self.spectrogram_transforms = spectrogram_transforms
 
@@ -107,8 +72,7 @@ class SpectrogramDataset(data.Dataset):
                     y = getattr(augment, func_name)(y)
                 except AttributeError as e:
                     print(f"{func_name} is None. {e}")
-
-        image = self.create_melspec_image(y)
+        image = self.transforms(y)
         labels = np.zeros(len(self.labels), dtype="f")
         labels[self.labels[label_code]] = 1
 
@@ -116,40 +80,3 @@ class SpectrogramDataset(data.Dataset):
             image = self.spectrogram_transforms(image)
 
         return image, labels
-
-    def create_melspec_image(self, y: np.array) -> np.array:
-        """信号データからメルスペクトログラム画像を生成する
-
-        Args:
-            y (np.array): 信号データ
-
-        Returns:
-            np.array: 画像データ
-        """
-        melspec1 = librosa.power_to_db(
-            librosa.feature.melspectrogram(
-                y, sr=self.target_sr, n_mels=self.n_mels, fmin=20, fmax=16000,
-                n_fft=self.target_sr//40, hop_length=self.target_sr//100
-            ), ref=np.max
-        ).astype(np.float32)
-
-        melspec2 = librosa.power_to_db(
-            librosa.feature.melspectrogram(
-                y, sr=self.target_sr, n_mels=self.n_mels, fmin=20, fmax=16000,
-                n_fft=self.target_sr//20, hop_length=self.target_sr//40
-            ), ref=np.max
-        ).astype(np.float32)
-
-        melspec3 = librosa.power_to_db(
-            librosa.feature.melspectrogram(
-                y, sr=self.target_sr, n_mels=self.n_mels, fmin=20, fmax=16000,
-                n_fft=self.target_sr//10, hop_length=self.target_sr//20
-            ), ref=np.max
-        ).astype(np.float32)
-
-        melspec1 = cv2.resize(melspec1, (250, 128))
-        melspec2 = cv2.resize(melspec2, (250, 128))
-        melspec3 = cv2.resize(melspec3, (250, 128))
-
-        melspec = np.stack([melspec1, melspec2, melspec3], axis=-1)
-        return normalize(melspec)
